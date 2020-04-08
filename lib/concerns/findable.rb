fd2fbe7eea7078421e5e8f64dc7bd1918526b995
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-04-08 20:32:01 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,5 +1,9 @@
 module Findale
-  def find_by_name
+  def find_by_name(name)
     @@artists.detect{|a| a.name == name}
+  end
+
+  def find_by_name(name)
+    @@songs.detect{|a| a.name == name}
   end
 end
